13d0649c1faac4cdec5adfc3e92f9621272b54b2
Joseph50079 <josephogbah2019@gmail.com>
2024-11-01 14:42:30 +0000

    ok push

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[float, str, bytes, int]) -> str:
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """ store() method for storing data
 
             Args:
@@ -30,3 +30,35 @@ class Cache:
         self._redis.mset(setd)
 
         return key
+
+    def get(self, key: str, fn: typing.Optional[typing.Callable] = None):
+        """ get() gets the value of the <key> in fn mode/format
+
+        Args:
+            key: key to value
+            fn: type format to convert value to
+        Return: return in fn format type
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_int(self, key: str) -> int:
+        """get_int() method get the value in integer
+        Args:
+            key: key to the value
+        Return: return in integer
+        """
+
+        return int(key)
+
+    def get_str(self, key: str) -> str:
+        """get_str() method to return value in str format
+        Args:
+            key: key to that value
+        Return: in strinf format
+        """
+
+        return str(key)
